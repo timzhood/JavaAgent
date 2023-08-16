ea4da9dfa1e64b87a9197476dc973c725f04c402
Tim Hood <tim.z.hood@gmail.com>
2023-08-16 23:03:00 +0100

    Whoops playing with dynamic dispatch.

--- Test-Transformers/src/com/timzh/mp/play/JimRecord002.java
+++ Test-Transformers/src/com/timzh/mp/play/JimRecord002.java
@@ -1,19 +1,21 @@
 package com.timzh.mp.play;
 
-public final class JimRecord002 extends BaseJimRecord002<JimRecord002>
+public final class JimRecord002
+extends
+BaseJimRecord002<JimRecord002>
 {
     public JimRecord002()
     {
         super();
     }
 
-    public JimRecord002(final BaseJimRecord001<?> that)
+    private JimRecord002(final BaseJimRecord001<?> that)
     {
         super(that);
         System.out.println("creating a JimRecord002 from a BaseJimRecord001");
     }
 
-    public JimRecord002(final BaseJimRecord002<?> that)
+    private JimRecord002(final BaseJimRecord002<?> that)
     {
         super(that);
         System.out.println("creating a JimRecord002 from a BaseJimRecord002");
@@ -23,6 +25,27 @@ public final class JimRecord002 extends BaseJimRecord002<JimRecord002>
     public JimRecord002 cloneRecord()
     {
         return new JimRecord002(this);
+    }
+
+    public static JimRecord002 create(final BaseJimRecord001<?> that)
+    {
+        final JimRecord002 result;
+
+        if (that instanceof BaseJimRecord002)
+        {
+            result = new JimRecord002((BaseJimRecord002<?>) that);
+        }
+        else if (that != null)
+        {
+            result = new JimRecord002(that);
+        }
+        else
+        {
+            result = null;
+            System.err.println("oops");
+        }
+
+        return result;
     }
 
 }
