d3d2b2b747dbd5c7d4ab9c90eb17e00aa87b3869
Tim Hood <tim.z.hood@gmail.com>
2024-08-16 17:43:05 +0100

    Made a new RCP app with the very latest Eclipse 2024-06.

---
+++ com.tzh.mp.generic.product/src/com/tzh/mp/generic/product/handlers/AboutHandler.java
@@ -0,0 +1,14 @@
+package com.tzh.mp.generic.product.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
+
+public class AboutHandler
+{
+    @Execute
+    public void execute(Shell shell)
+    {
+        MessageDialog.openInformation(shell, "About", "Eclipse RCP Application");
+    }
+}
